67702e2736bd92bd19af1b24d9260ce1346ba1e7
Josu Vazquez <josu9513@gmail.com>
2022-11-10 19:49:26 +0100

    Update product

--- models/Product.js
+++ models/Product.js
@@ -2,15 +2,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
-    name: {
+    title: {
+        type: String,
+    },
+    category: {
         type: String,
     },
     price: {
-        type: String
+        type: Number
     },
     description: {
         type: String
-    }
+    },
+    currency: {
+        type: String,
+    },
 });
 
 module.exports = mongoose.model('product', productSchema);
